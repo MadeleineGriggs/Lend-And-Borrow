1a18ff388cb304edb621ba4d96b990833ab241ac
cjstanzhu <cjstanzhu@gmail.com>
2019-09-14 12:25:05 -0400

    setup signup api route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,43 @@
-
 var db = require("../models");
+var passport = require("../config/passport");
 
 module.exports = function(app) {
     // Get all examples
-    app.get("/api/examples", function(req, res) {
-        db.Example.findAll({}).then(function(dbExamples) {
-            res.json(dbExamples);
-        });
-    });
+    // app.get("/api/examples", function(req, res) {
+    //     db.Example.findAll({}).then(function(dbExamples) {
+    //         res.json(dbExamples);
+    //     });
+    // });
 
     // Create a new example
-    app.post("/api/examples", function(req, res) {
-        db.Example.create(req.body).then(function(dbExample) {
-            res.json(dbExample);
+    // app.post("/api/examples", function(req, res) {
+    //     db.Example.create(req.body).then(function(dbExample) {
+    //         res.json(dbExample);
+    //     });
+    // });
+
+    app.post("/api/login", passport.authenticate("local"), function(req, res) {
+        res.json(req.user);
+    });
+
+    app.post("/api/signup", function(req, res) {
+        db.User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        }).then(function() {
+            res.redirect(307, "/api/login");
+        }).catch(function(err) {
+            res.status(401).json(err);
         });
+
     });
 
     // Delete an example by id
-    app.delete("/api/examples/:id", function(req, res) {
-        db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-            res.json(dbExample);
-        });
-    });
+    // app.delete("/api/examples/:id", function(req, res) {
+    //     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+    //         res.json(dbExample);
+    //     });
+    // });
 };
+
