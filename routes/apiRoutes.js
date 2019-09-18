c6c7feaed3c783cd3170e964fb2eebb3ff4bee72
Madeleine Griggs <madeleinegriggs@gmail.com>
2019-09-17 23:11:30 -0400

    fixed it so users and items are working properly. Got one item unique to the user to partially display.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -22,7 +22,6 @@ module.exports = function(app) {
 
     //Route for creating Items.
     app.post("/api/items", function(req, res) {
-
         db.Item.create({
             title: req.body.title,
             body: req.body.body,
@@ -49,48 +48,18 @@ module.exports = function(app) {
         }
     });
 
-
-    // app.get("/dashboard", isAuthenticated, function(req, res) {
-    //     db.item.findAll({
-    //         where: {UserId: req.user.id},
-    //         order: [["createdAt", "DESC"]]
-
-    //     }).then(function(dbItems) {
-    //         var hbsObject = {
-    //             items: dbItems
-    //         };
-
-    //         res.render("dashboard", hbsObject);
-
-    //     });
-
-
-
-    app.get("/", function(req, res) {
-
-        db.Item.findAll({
-            order: [["createdAt", "DESC"]]
-
-        }).then(function(dbItems) {
-            var newItems = {
-                items: dbItems
-            };
-
-            res.render("index", newItems);
-
-        });
+    app.get("/api/user_data", function(req, res) {
+        if (!req.user) {
+        // The user is not logged in, send back an empty object
+            res.json({});
+        } else {
+        // Otherwise send back the user's email and id
+            res.json({
+                username: req.user.username,
+                email: req.user.email,
+                id: req.user.id
+            });
+        }
     });
 
-    app.post("/", function(req, res) {
-        db.User.create({
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password
-        }).then(function() {
-            res.redirect(307, "/dashboard");
-        }).catch(function(err) {
-            res.status(401).json(err);
-        });
-
-    });
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,25 +5,56 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
     // Load index page
+    // app.get("/", function(req, res) {
+    //     db.Example.findAll({}).then(function(dbExamples) {
+    //         res.render("index", {
+    //             msg: "Welcome!",
+    //             examples: dbExamples
+    //         });
+    //     });
+    // });
+
+
     app.get("/", function(req, res) {
-        db.Example.findAll({}).then(function(dbExamples) {
-            res.render("index", {
-                msg: "Welcome!",
-                examples: dbExamples
-            });
-        });
+
+        res.render("index");
+
     });
+
+    // db.Item.findAll({
+    //     order: [["createdAt", "DESC"]]
+
+    // }).then(function(dbItems) {
+    //     var newItems = {
+    //         items: dbItems
+    //     };
+
+    //     res.render("index", newItems);
+
+    // });
+
 
     app.get("/dashboard", function(req, res) {
         if(req.isAuthenticated()){
-            db.User.findOne({where: {username: req.user.username}}).then(function(dbUser) {
+            db.Item.findOne({where: {UserId: req.user.id}}).then(function(dbUser) {
                 res.render("dashboard", {
                     user: dbUser
                 });
             });
+        
+            // db.Item.findOne({where: {userId: req.user.id}}).then(function(dbItems) {
+
+            //     res.render("dashboard", {
+            //         items: dbItems
+            //     });
+
+            // });
+            // });
+
         } else {
             res.render("index");
         }
+        // res.render("dashboard");
     });
   
 
