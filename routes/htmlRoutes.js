4063d9f6c9593be08b21e872aa3238e5ac85d884
Angela Jane Leung <angelajaneleung@gmail.com>
2019-09-17 20:12:58 -0400

    s

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -20,6 +20,10 @@ module.exports = function(app) {
         res.render("dashboard");
     });
 
+    app.get("/select", function(req, res) {
+        res.render("select");
+    });
+
     // Load example page and pass in an example by id
     app.get("/example/:id", function(req, res) {
         db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
